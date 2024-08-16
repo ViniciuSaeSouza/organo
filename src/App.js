e4d2c961824f074616f4e51bc9c750bf40053fbf
Vinicius Saes <viniciusaesouza@gmail.com>
2024-08-15 21:47:45 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import Banner from './componentes/Banner/Banner';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <Banner/>
+      <header className="App-header"> 
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Bem vindo ao react.
         </p>
         <a
           className="App-link"
