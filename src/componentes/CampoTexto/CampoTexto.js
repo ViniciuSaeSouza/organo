322e0a1faafefb15f63370b3b2daab77fef7bc44
Vinicius Saes <viniciusaesouza@gmail.com>
2024-09-01 19:22:20 -0300

    campoTexto

---
+++ src/componentes/CampoTexto/CampoTexto.js
@@ -0,0 +1,14 @@
+import './CampoTexto.css'
+
+const CampoTexto = () => {
+  return(
+    <>
+    <div className='campoTexto'>
+      <label>Nome</label>
+      <input />
+    </div>
+    </>
+  );
+};
+
+export default CampoTexto;
